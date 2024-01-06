7e2383d783a1645681418d6adcdcf764ad5da062
Tiger Abrodi <tigerabrodi@gmail.com>
2024-01-06 18:11:15 +0100

    playing around

--- first-exercise/index.js
+++ first-exercise/index.js
@@ -1,19 +1,27 @@
 const { slowFunction } = require("./slowFunction");
-const { performance } = require("perf_hooks");
+const { performance, PerformanceObserver } = require("perf_hooks");
 
 function main() {
   const arraySize = 100;
 
+  // Create an observer for performance measures and log them
+  const obs = new PerformanceObserver((items) => {
+    items.getEntries().forEach((entry) => {
+      console.log(`${entry.name}: ${entry.duration} milliseconds`);
+    });
+  });
+  obs.observe({ entryTypes: ["measure"] });
+
   // Start measuring time
   performance.mark("startTask");
-  // ... task code ...
 
   // Call the slow function
   const result = slowFunction(arraySize);
 
+  // End measuring time
   performance.mark("endTask");
 
-  // Stop measuring time
+  // Measure the duration between marks
   performance.measure("myTask", "startTask", "endTask");
 }
 
